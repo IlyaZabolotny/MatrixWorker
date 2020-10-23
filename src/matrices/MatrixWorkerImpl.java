4f80d70a49726233d4ab0658d79ee93c53930823
Ilya <picasso.ilya@mail.ru>
2020-10-23 14:41:18 +0300

    add determinant calculation method

--- src/matrices/MatrixWorkerImpl.java
+++ src/matrices/MatrixWorkerImpl.java
@@ -9,7 +9,7 @@ package matrices;
 public class MatrixWorkerImpl implements IMatrixWorker{
 
     @Override
-    public void print(double[][] m){
+    public void print(double[][] m) {
         for (int i=0; i < m.length; i++){
             for (int j = 0; j < m[0].length; j++){
                 System.out.print(m[i][j]+" ");
@@ -19,7 +19,7 @@ public class MatrixWorkerImpl implements IMatrixWorker{
     }
 
     @Override
-    public boolean haveSameDimension(double[][] m1, double[][] m2){
+    public boolean haveSameDimension(double[][] m1, double[][] m2) {
         return ((m1.length == m2.length) && (m1[0].length == m2[0].length));
     }
 
@@ -56,7 +56,7 @@ public class MatrixWorkerImpl implements IMatrixWorker{
     }
 
     @Override
-    public double[][] multiply(double[][] m1, double[][] m2){
+    public double[][] multiply(double[][] m1, double[][] m2) {
         if (m1[0].length == m2.length){
             double[][] m = new double[m1.length][m2[0].length];
             for (int i = 0; i < m1.length; i++){
@@ -72,5 +72,51 @@ public class MatrixWorkerImpl implements IMatrixWorker{
         } else {
             throw new IllegalArgumentException ("Matrix m1's columns amount must be same as m2's rows amount");
         }
+    }
+
+
+    /**
+     * Calculates the determinant of N*N matrix m. The matrix should be square.
+     * @param m
+     * @param N - the dimension of m.
+     * @return determinant
+     * @throws IllegalArgumentException if the matrix is not square.
+     */
+    public double determinant(double[][] m, int N) throws IllegalArgumentException {
+        double res;
+        double m1[][];
+        if (m.length != m[0].length) {
+            throw new IllegalArgumentException ("This is not a square matrix.");
+        } else {
+            if (N == 1) {
+                res = m[0][0];
+            } else if (N == 2){
+                res = m[0][0]*m[1][1] - m[1][0]*m[0][1];
+            } else {
+                res=0;
+                for (int j1=0; j1<N; j1++){
+                    m1 = generateSubArray (m, N, j1);
+                    res += Math.pow(-1.0, 1.0+j1+1.0) * m[0][j1] * determinant(m1, N-1);
+                }
+            }
+            return res;
+        }
+    }
+
+    public double[][] generateSubArray (double[][] m, int N, int j1) {
+        double[][] m1 = new double[N-1][];
+        for (int k=0; k<(N-1); k++) {
+            m1[k] = new double[N - 1];
+        }
+        for (int i=1; i<N; i++){
+            int j2=0;
+            for (int j=0; j<N; j++){
+                if(j == j1)
+                    continue;
+                m1[i-1][j2] = m[i][j];
+                j2++;
+            }
+        }
+        return m1;
     }
 }
