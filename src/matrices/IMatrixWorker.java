5affbaa63ec8615b5552386bfe070b73ba729426
Ilya <picasso.ilya@mail.ru>
2020-10-23 12:27:25 +0300

    add the basic functionality of the class

--- src/matrices/IMatrixWorker.java
+++ src/matrices/IMatrixWorker.java
@@ -1,9 +1,44 @@
 package matrices;
 
 public interface IMatrixWorker {
+
+    /**
+     *Prints the matrix to console.
+     * @param m
+     */
     public void print(double[][] m);
+
+    /**
+     * Checks whether the matrix m1 has same dimension as the matrix m2
+     * @param m1
+     * @param m2
+     */
     public boolean haveSameDimension(double[][] m1, double[][] m2);
-    //public double[][] add(double[][] m1, double[][] m2);
-    //public double[][] subtract(double[][] m1, double[][] m2);
-    //public double[][] multiply(double[][] m1, double[][] m2);
+
+    /**
+     *Adds the matrices m1 and m2 if their dimensions are the same.
+     * @param m1
+     * @param m2
+     * @return the sum of the matrices
+     * @throws IllegalArgumentException if dimensions are not the same.
+     */
+    public double[][] add(double[][] m1, double[][] m2);
+
+    /**
+     *Subtracts the matrix m2 from the matrix m1 if their dimensions are the same.
+     * @param m1
+     * @param m2
+     * @return the subtraction of matrices m1 and m2
+     * @throws IllegalArgumentException if dimensions are not the same.
+     */
+    public double[][] subtract(double[][] m1, double[][] m2);
+
+    /**
+     *Multiplies two matrices m1 and m2 if m1 and m2 are set properly.
+     * @param m1
+     * @param m2
+     * @return the matrix product.
+     * @throws IllegalArgumentException if the number of columns of m1 doesn't equal to the number of rows of m2.
+     */
+    public double[][] multiply(double[][] m1, double[][] m2);
 }
--- src/matrices/MatrixWorkerImpl.java
+++ src/matrices/MatrixWorkerImpl.java
@@ -8,10 +8,7 @@ package matrices;
  */
 public class MatrixWorkerImpl implements IMatrixWorker{
 
-    /**
-     *Prints the matrix to console.
-     * @param m
-     */
+    @Override
     public void print(double[][] m){
         for (int i=0; i < m.length; i++){
             for (int j = 0; j < m[0].length; j++){
@@ -21,15 +18,59 @@ public class MatrixWorkerImpl implements IMatrixWorker{
         }
     }
 
-    /**
-     * Checks whether matrix m1 has same dimension as matrix m2
-     * @param m1
-     * @param m2
-     */
+    @Override
     public boolean haveSameDimension(double[][] m1, double[][] m2){
         return ((m1.length == m2.length) && (m1[0].length == m2[0].length));
     }
 
+    @Override
+    public double[][] add(double[][] m1, double[][] m2) {
+        MatrixWorkerImpl matrixWorker = new MatrixWorkerImpl();
+        if (matrixWorker.haveSameDimension(m1, m2)){
+            double[][] m = new double[m1.length][m1[0].length];
+            for (int i = 0; i < m1.length; i++){
+                for (int j = 0; j < m1[0].length; j++){
+                    m[i][j] = m1[i][j] + m2[i][j];
+                }
+            }
+            return m;
+        } else {
+            throw new IllegalArgumentException("Dimensions should be the same.");
+        }
+    }
 
+    @Override
+    public double[][] subtract(double[][] m1, double[][] m2) {
+        MatrixWorkerImpl matrixWorker = new MatrixWorkerImpl();
+        if (matrixWorker.haveSameDimension(m1, m2)){
+            double[][] m = new double[m1[0].length][m1[0].length];
+            for (int i = 0; i < m1.length; i++){
+                for (int j = 0; j < m1[0].length; j++){
+                    m[i][j] = m1[i][j] - m2[i][j];
+                }
+            }
+            return m;
+        } else {
+            throw new IllegalArgumentException("Dimensions should be the same.");
+        }
+    }
 
+    @Override
+    public double[][] multiply(double[][] m1, double[][] m2){
+        if (m1[0].length == m2.length){
+            double[][] m = new double[m1.length][m2[0].length];
+            for (int i = 0; i < m1.length; i++){
+                for (int j = 0; j < m2[0].length; j++){
+                    double temp = 0;
+                    for (int k = 0; k < m1.length; k++){
+                        temp += m1[k][i] * m2[j][k];
+                    }
+                    m[i][j] = temp;
+                }
+            }
+            return m;
+        } else {
+            throw new IllegalArgumentException ("Matrix m1's columns amount must be same as m2's rows amount");
+        }
+    }
 }
